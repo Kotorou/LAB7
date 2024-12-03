949780fd6f9ce0566ac8b14aa13b802d2347eb7c
Kotaro <kota.kawaka@gmail.com_>
2024-12-03 14:47:43 +0700

    Lab7.1 Finished

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
-
-    outpot = a - b/c;
-    count << output: << output << \n;
+    float a  =  30.0, b = 5.0,c = 2.0, output;
+    output = (a - b)/c;
+    cout << "output: "<< output << "\n";
     return 0;
 }
