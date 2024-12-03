d8234d3ec7f5ee01923809d08e05c4780edc9093
Kotaro <kota.kawaka@gmail.com_>
2024-12-03 21:22:15 +0700

    lab7.2 finish

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int main(){
  string name,movie,date,bye;
- int studentnum = 0;
+ int studentnum ;
 cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
 cout << "?????: ";
-cin >> name;
+getline(cin,name);
 cout <<"Fahsai: Wow!!! " << name << " is a really cool name.\n";
 cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
 cin >> studentnum;
 cin.ignore();
-cout << name <<  ": " << studentnum ;
-cout << "Fahsai: I think you may be GEAR " << (studentnum / 10000000) -12 <<" I have a free movie ticket for you.\n";
+cout << name <<  ": " << studentnum << "\n" ;
+cout << "Fahsai: I think you may be GEAR " << (studentnum / 10000000) -12 <<". I have a free movie ticket for you.\n";
 cout << "Fahsai: Let's go to the cinema together!!!\n";
 cout << "Fahsai: What movie do you want to watch?\n";
+cout << name << ": ";
 getline(cin,movie);
-cout << name << ": " << movie <<"\n";
 cout << "Fahsai: So....which day are you free to go with me?\n";
+cout << name <<  ": ";
 getline(cin,date);
-cout << name <<  ": " << date << "\n";
-cout << "Fahsai: " << date << "....that is OK!!! I'm looking forward to watching Star Wars VII with you.\n";
+cout << "Fahsai: " << date << "....that is OK!!! I'm looking forward to watching "<< movie << " with you.\n";
+cout << name << ": ";
 getline(cin,bye);
-cout << name << ": " << bye << "\n";
-cout << "Fahsai: 555+ see you Next Monday. Bye Bye \\(^ ^)/\n";
+cout << "Fahsai: 555+ see you "<< date<<". Bye Bye \\(^ ^)/\n";
 }
